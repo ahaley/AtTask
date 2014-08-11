6a52af01765a47c82222b36d9f66bf59120c6da8
Antonio Haley <antonio.haley@gmail.com>
2014-08-11 10:53:46 -0400

    Added enhanced per diem
    Added enhcaned per diem struct

--- ahaley.AtTask/ExpenseType.cs
+++ ahaley.AtTask/ExpenseType.cs
@@ -7,5 +7,6 @@ namespace ahaley.AtTask
         public static readonly string InChargeExpenseType = "9d3c90342fe3fa2ae040007f01002426";
         public static readonly string MileageExpenseType = "9d3c90342fc8fa2ae040007f01002426";
         public static readonly string PerDiem = "9d3c90342fe4fa2ae040007f01002426";
+        public static readonly string EnhancedPerDiem = "5386621300443537f9f86ec83360d782";
     }
 }
